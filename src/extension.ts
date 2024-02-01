0820823fc466a866b095b76f7867dcd1c69a5023
ohamill <owen.oxley.hamill@gmail.com>
2024-02-01 08:53:13 +0100

    Add `aura new` command

--- src/extension.ts
+++ src/extension.ts
@@ -13,16 +13,16 @@ export function activate(context: vscode.ExtensionContext) {
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with registerCommand
 	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('aura-lang.helloWorld', () => {
+	let disposable = vscode.commands.registerCommand('aura-lang.new', () => {
 		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from aura-lang!');
-        let input = vscode.window.showInputBox({
-            prompt: "new",
-            title: "Project Name",
-            value: "Playground"
+        vscode.window.showInputBox({
+            prompt: "Enter project name",
+            validateInput: (value: string): string => {
+                if (value === "") { return "Project's name cannot be empty."; }
+                else { return ""; }
+            }
         }).then(input => {
-            vscode.window.showInformationMessage(input || "");
+            vscode.window.showInformationMessage(`You entered: ${input || ""}`);
         });
 	});
 
