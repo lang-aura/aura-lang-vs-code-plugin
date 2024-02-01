f08f9105ec11e3f561c9e2848548fa80e01b6f00
Owen Hamill <47223204+ohamill@users.noreply.github.com>
2024-02-01 13:17:29 +0100

    Add `aura new` command (#1)
    * Add `aura new` command
    
    * Add `aura new` command

--- src/extension.ts
+++ src/extension.ts
@@ -1,6 +1,7 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
+import { ExecException, exec } from 'node:child_process';
 
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
@@ -13,16 +14,38 @@ export function activate(context: vscode.ExtensionContext) {
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with registerCommand
 	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('aura-lang.helloWorld', () => {
+	let disposable = vscode.commands.registerCommand('aura-lang.new', () => {
 		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from aura-lang!');
-        let input = vscode.window.showInputBox({
-            prompt: "new",
-            title: "Project Name",
-            value: "Playground"
-        }).then(input => {
-            vscode.window.showInformationMessage(input || "");
+        vscode.window.showInputBox({
+            prompt: "Enter project name",
+            validateInput: (value: string): string => {
+                if (value === "") { return "Project's name cannot be empty."; }
+                else { return ""; }
+            }
+        }).then((projectName: string | undefined) => {
+            vscode.window.showOpenDialog({
+                canSelectFiles: false,
+                canSelectFolders: true,
+                canSelectMany: false,
+                defaultUri: vscode.workspace.workspaceFolders !== undefined
+                                ? vscode.Uri.parse(vscode.workspace.workspaceFolders[0].uri.path)
+                                : undefined,
+                openLabel: "Select project directory",
+            }).then((selected: vscode.Uri[] | undefined) => {
+                if (selected && selected[0] !== undefined) {
+                    exec(`aura new ${projectName} -o ${selected[0].fsPath}`, (err: ExecException | null, _: string, stderr: string) => {
+                        if (err !== null) {
+                            vscode.window.showErrorMessage(err.message);
+                        } else if (stderr !== "") {
+                            vscode.window.showErrorMessage(stderr);
+                        } else {
+                            vscode.commands.executeCommand('vscode.openFolder', vscode.Uri.parse(`${selected[0].fsPath}/${projectName}`));
+                        }
+                    });
+                } else {
+                    vscode.window.showErrorMessage("Must select a directory for the new Aura project");
+                }
+            });
         });
 	});
 
