0e6f6c92c4159018d67524bd912eaed8767ddff1
Owen Hamill <owen.hamill@devoteam.com>
2024-02-03 09:27:54 +0100

    Update linter

--- src/test/extension.test.ts
+++ src/test/extension.test.ts
@@ -5,9 +5,11 @@ import * as assert from 'assert';
 import * as vscode from 'vscode';
 // import * as myExtension from '../../extension';
 
+// eslint-disable-next-line no-undef
 suite('Extension Test Suite', () => {
 	vscode.window.showInformationMessage('Start all tests.');
 
+	// eslint-disable-next-line no-undef
 	test('Sample test', () => {
 		assert.strictEqual(-1, [1, 2, 3].indexOf(5));
 		assert.strictEqual(-1, [1, 2, 3].indexOf(0));
