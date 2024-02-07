0655e3e12f94726be839b12bdf33b81b817e91f5
Owen Hamill <47223204+ohamill@users.noreply.github.com>
2024-02-07 10:34:18 +0100

    Add LSP client and server (#11)

--- src/extension.ts
+++ src/extension.ts
@@ -3,7 +3,10 @@ import { RunVSCodeCommand } from './commands/run';
 import { FmtVSCodeCommand } from './commands/fmt';
 import { NewVSCodeCommand } from './commands/new';
 import { AuraVSCodeCommand } from './commands/command';
-import { ExtensionContext, commands } from 'vscode';
+import { ExtensionContext, commands, workspace } from 'vscode';
+import { LanguageClient, LanguageClientOptions, ServerOptions } from 'vscode-languageclient/node';
+
+let client: LanguageClient;
 
 export function activate(context: ExtensionContext) {
     // Map each of the registered commands with the appropriate handler
@@ -20,7 +23,37 @@ export function activate(context: ExtensionContext) {
         });
         context.subscriptions.push(disposable);
     });
+
+    client = initLanguageClient();
+    // Start the client. This will also launch the server
+    client.start();
 }
 
 // This method is called when your extension is deactivated
-export function deactivate() {}
+export function deactivate() {
+    return client?.stop();
+}
+
+function initLanguageClient(): LanguageClient {
+    let clientOptions: LanguageClientOptions = {
+        documentSelector: [{ scheme: "file", language: "aura" }],
+        synchronize: {
+            fileEvents: [
+                workspace.createFileSystemWatcher("**/aura.toml"),
+                workspace.createFileSystemWatcher("**/*.aura")
+            ],
+        },
+    };
+
+    let serverOptions: ServerOptions = {
+        command: "aura",
+        args: [ "lsp" ]
+    };
+
+    return new LanguageClient(
+        "aura-language-server",
+        "Aura Language Server",
+        serverOptions,
+        clientOptions,
+    );
+}
